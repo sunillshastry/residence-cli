a072ed0f1bc5d408b65cc30c5552f0240d9a6eee
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-03 16:14:02 +0530

    Implemented the get_name method for Residence class

--- src/Residence.cpp
+++ src/Residence.cpp
@@ -15,3 +15,8 @@ Residence::Residence(std::string name, int min_bed_label, int max_bed_label)
 	this->min_bed_label = min_bed_label;
 	this->max_bed_label = max_bed_label;
 }
+
+std::string Residence::get_name()
+{
+	return this->name;
+}
