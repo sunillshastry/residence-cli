8df5e0c880b7e2411383de47681399c02f8d4d51
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-03 16:12:59 +0530

    Added two constructor methods to Residence class

---
+++ src/Residence.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "./../include/Residence.h"
+
+Residence::Residence()
+{
+	this->name = "";
+	this->min_bed_label = 0;
+	this->max_bed_label = 1;
+}
+
+Residence::Residence(std::string name, int min_bed_label, int max_bed_label)
+{
+	this->name = name;
+	this->min_bed_label = min_bed_label;
+	this->max_bed_label = max_bed_label;
+}
