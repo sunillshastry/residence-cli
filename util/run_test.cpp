e10ec46cc067ca71de0fd0d2a04fce420b1e0f38
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-04 20:23:18 +0530

    Updated the function doc for string:run_test.cpp

--- util/run_test.cpp
+++ util/run_test.cpp
@@ -2,11 +2,11 @@
 #include "run_test.h"
 
 /**
- * Compares the given values and displays the according message.
+ * Compares two given string values and displays the according message.
  * Tests only for strings values
- * @param expected The user-expected value
- * @param result The value returned from the function/method
- * @param method The name of the method/function with the class name.
+ * @param expected The user-expected value (string)
+ * @param result The value returned from the function/method (string)
+ * @param method The name of the method/function with the class name. (string)
  */
 void run_test(std::string expected, std::string result, std::string method = "(Method not available)")
 {
