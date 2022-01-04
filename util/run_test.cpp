8b5b1ac96ca720100420913f7a100e2bb416fbdc
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-04 20:24:56 +0530

    Implemented the run_test method to test two integer values

--- util/run_test.cpp
+++ util/run_test.cpp
@@ -21,3 +21,24 @@ void run_test(std::string expected, std::string result, std::string method = "(M
 		std::cout << "TEST PASSED!" << std::endl;
 	}
 }
+
+/**
+ * Compares two given integer values and displays the according message.
+ * Tests only for strings values
+ * @param expected The user-expected value (integer)
+ * @param result The value returned from the function/method (integer)
+ * @param method The name of the method/function with the class name. (string)
+ */
+void run_test(int expected, int result, std::string method = "(Method not available)")
+{
+	if (expected != result)
+	{
+		std::cout << "TEST FAILED!" << std::endl;
+		std::cout << "Expected: " << expected << ". Result: " << result << std::endl;
+		std::cout << "Bug detected in" << method << " method" << std::endl;
+	}
+	else
+	{
+		std::cout << "TEST PASSED!" << std::endl;
+	}
+}
