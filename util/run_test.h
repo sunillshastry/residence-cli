4667ba63c7ebcc86b64e10df88b82796c2dd6c3d
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-04 22:20:11 +0530

    Updated the function prototype for run_test. Included a new parameter with a default value

--- util/run_test.h
+++ util/run_test.h
@@ -4,15 +4,15 @@
 #define __RUN_TEST_H__
 
 /** Tests two strings to check if the test was successful or not */
-void run_test(std::string expected, std::string result);
+void run_test(std::string expected, std::string result, std::string method = "(Method not available)");
 
 /** Tests two integers to check if the test was successful or not */
-void run_test(int expected, int result);
+void run_test(int expected, int result, std::string method = "(Method not available)");
 
 /** Tests two booleans to check if the test was successful or not */
-void run_test(bool expected, bool result);
+void run_test(bool expected, bool result, std::string method = "(Method not available)");
 
 /** Tests two floating values to check if the test was successful or not */
-void run_test(double expected, double result);
+void run_test(double expected, double result, std::string method = "(Method not available)");
 
 #endif
