eae92c4c93b925eef03b79180cd7f973b85ca3e7
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-04 20:08:29 +0530

    Implemented the to_string method in BasicManager.cpp class. Working perfectly well without any ignored lines

--- src/BasicManager.cpp
+++ src/BasicManager.cpp
@@ -17,3 +17,11 @@ std::string BasicManager::get_employee_id()
 {
 	return this->employee_id;
 }
+
+std::string BasicManager::to_string()
+{
+	std::string result = "Name: " + this->get_name() + "\n";
+	result += "Social Insurance Number: " + this->get_social_insurance_number() + "\n";
+	result += "Employee ID: " + this->employee_id + "\n";
+	return result;
+}
