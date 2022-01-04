503df65ccabc3bb7fcb8e98c131ea1c0be9c3154
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-04 19:05:13 +0530

    Created the Student class prototype with attributes and methods. Some methods have been left ignored due to dependency on other classes

---
+++ include/Student.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include "Person.h"
+
+#ifndef __STUDENT_H__
+#define __STUDENT_H__
+
+class Student : public Person
+{
+private:
+	std::string name;
+	std::string social_insurance_number;
+	std::string student_id;
+	int bed_label;
+	// vector<Manager> managers_list;
+
+public:
+	Student();
+	Student(std::string name, std::string social_insurance_number);
+
+	int get_bed_label();
+	std::string get_id();
+
+	// void add_manager(Manager manager);
+	// void remove_manager(std::string employee_id);
+	// bool has_manager(std::string employee_id);
+	// vector<Manager> get_manager_list();
+
+	void set_bed_label(int bed_label);
+
+	std::string to_string();
+};
+
+#endif
