4f3d986cf507acf81330da9d0962500d52f30bed
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-05 15:46:59 +0530

    Added the first test-case for BasicManager. Test successful

--- tests/BasicManagerTest.cpp
+++ tests/BasicManagerTest.cpp
@@ -9,5 +9,15 @@ using std::string;
 
 int main()
 {
+	string expected = "", result = "";
+
+	// Create a BasicManager
+	BasicManager basic_manager("Hagrid", "gryffindor123", "gry555");
+
+	// TEST 1
+	expected = "Hagrid";
+	result = basic_manager.get_name();
+	run_test(expected, result, "BasicManager.get_name() : Person");
+
 	return 0;
 }
