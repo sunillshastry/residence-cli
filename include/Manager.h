e15b91ac1e5748de3abcb0b684ce5ba50f972cf6
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-05 16:19:04 +0530

    Added the entire class prototype for Manager class. No ignored attributes or methods. All dependences are in place

--- include/Manager.h
+++ include/Manager.h
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <vector>
 #include "BasicManager.h"
+#include "Student.h"
 
 #ifndef __MANAGER_H__
 #define __MANAGER_H__
 
 class Manager : public BasicManager
 {
+private:
+	std::vector<Student> students_list;
+
+public:
+	Manager();
+	Manager(std::string name, std::string social_insurance_number, std::string employee_id);
+
+	void add_student(Student student);
+	void remove_student(std::string student_id);
+	bool has_student(std::string student_id);
+
+	std::string to_string();
 };
 
 #endif
