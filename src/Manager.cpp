926f8a4f8d3b5752489d799027023c734fe994b5
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-05 16:34:57 +0530

    Implemented the has_student method for Manager class

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -13,3 +13,16 @@ void Manager::add_student(Student student)
 {
 	this->students_list.push_back(student);
 }
+
+bool Manager::has_student(std::string student_id)
+{
+	for (int i = 0; i < this->students_list.size(); i++)
+	{
+		std::string iterative_id = this->students_list.at(i).get_id();
+		if (student_id == iterative_id)
+		{
+			return true;
+		}
+	}
+	return false;
+}
