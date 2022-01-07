185da5db3dc6b9a3c60984025a1d821b48ad2b91
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-07 18:34:53 +0530

    Documented the Student class constructors inside the Student.cpp file

--- src/Student.cpp
+++ src/Student.cpp
@@ -2,12 +2,24 @@
 #include "./../include/Student.h"
 #include "./../include/Person.h"
 
+/**
+ * The default constructor for the Student class, creates a Student object with the unique student identity
+ * number that is necessary.
+ * @param student_id The unique identity number of the student object.
+ */
 Student::Student(std::string student_id) : Person()
 {
 	this->student_id = student_id;
 	this->bed_label = -1;
 }
 
+/**
+ * The main constructor for the Student class, creates a Student object with the name, social insurance
+ * number and student identity number to create a proper Student object.
+ * @param name The name of the Student
+ * @param social_insurance_number The social insurance number of the Student
+ * @param student_id The unique identity number of the Student
+ */
 Student::Student(std::string name, std::string social_insurance_number, std::string student_id)
 		: Person(name, social_insurance_number)
 {
