6aea8cdcfead0a402904fe6828cb17cd360528f7
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-07 18:21:23 +0530

    Completed the entire Student.h class prototype for the Student class. File is not documented

--- include/Student.h
+++ include/Student.h
@@ -9,6 +9,21 @@ class Student : public Person
 private:
 	int bed_label;
 	std::string student_id;
+
+public:
+	// Constructor
+	Student(std::string student_id);
+	Student(std::string name, std::string social_insurance_number, std::string student_id);
+
+	// Accessor methods
+	int get_bed_label();
+	std::string get_student_id();
+
+	// Mutator methods
+	void set_bed_label(int bed_label);
+
+	// Summary methods
+	std::string to_string();
 };
 
 #endif
