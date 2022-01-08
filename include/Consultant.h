e57da61da1676daa66314ac6aa2d6f396e7526de
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-08 19:12:28 +0530

    Documented the Consultant.h header class

--- include/Consultant.h
+++ include/Consultant.h
@@ -4,12 +4,20 @@
 #define __CONSULTANT_H__
 #include "Manager.h"
 
+/**
+ * A model of the Consultant object, a less advanced version of the Manager object.
+ * The class inherits from Manager class.
+ */
 class Consultant : public Manager
 {
 public:
+	/** The default constructor for the Consultant class, initialises all values to null or zero */
 	Consultant();
+
+	/** The main constructor for the Consultant class, initialises all values to their respective values*/
 	Consultant(std::string name, std::string social_insurance_number, std::string employee_id);
 
+	/** Returns a string representation of the Consultant including all the details of the consultant */
 	std::string to_string();
 }
 
