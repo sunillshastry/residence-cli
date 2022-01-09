4981f92670c14e9f55a7b190ca12d52fbb2643e9
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-09 21:39:35 +0530

    Created a test blueprint for ConsultantTest file

---
+++ tests/ConsultantTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "./../include/Consultant.h"
+#include "./../util/run_test.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+
+	return 0;
+}
