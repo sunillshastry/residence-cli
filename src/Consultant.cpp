0512bc88a05d86c2c32976dcd63fddcfa91088c6
Sunil Shastry <sunil.shastry69@gmail.com>
2022-01-09 21:22:56 +0530

    Implemented the to_string method in the Consultant class, inside the Consultant.cpp file

--- src/Consultant.cpp
+++ src/Consultant.cpp
@@ -5,3 +5,12 @@ Consultant::Consultant() : Manager() {}
 
 Consultant::Consultant(std::string name, std::string social_insurance_number, std::string employee_id)
 		: Manager(name, social_insurance_number, employee_id) {}
+
+std::string Consultant::to_string()
+{
+	std::string result = "Name: " + this->get_name() + "\n";
+	result += "Social Insurance Number: " + this->get_social_insurance_number() + "\n";
+	result += "Employee ID: " + this->get_employee_id() + "\n";
+	result += "Consultant: Yes\n";
+	return result;
+}
